520855d3793779ec924e2a76ec125ea87876667a
dave-lence <davidfnwudo@gmail.com>
2023-10-09 23:57:56 +0100

    initial push

---
+++ grocery_shopping/screens/intro_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatefulWidget {
+  const IntroScreen({super.key});
+
+  @override
+  State<IntroScreen> createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+       body:Container(
+        child: Text("home "),
+       ),
+     );
+  }
+}
